8ec2a1e3236fc4e1283e99f85114b071967a5142
jic900 <jiachen.can@gmail.com>
2017-01-05 20:45:04 -0700

    Add files via upload

--- WebApplication/src/atb/test/webapp/util/UserLoginUtil.java
+++ WebApplication/src/atb/test/webapp/util/UserLoginUtil.java
@@ -1,22 +1,23 @@
 package atb.test.webapp.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.regex.Pattern;
 
 public class UserLoginUtil {
-	
-	public static final String ODATA_SERVICE_URL = "http://localhost:8080/WebApplication/odata";
-	public static final String GOOGLE_CSE_API_URL = "https://www.googleapis.com/customsearch/v1?key=AIzaSyA0eV_mQ5sXMnHKf_qDKsdfRDU1NrK94g0&cx=017337551614757810777:tam1j_phddi";
-	
+
 	public static final String USER_NAME = "UserName";
 	public static final String USER_PASSWORD = "Password";
-	
+
 	/**
-	 * User password validation implementation 
+	 * User password validation implementation
+	 * 
 	 * @param password
 	 * @return validation message
 	 */
-	public static String ValidateUserPassword(String password){
-		
+	public static String ValidateUserPassword(String password) {
+
 		String errorMsg = "";
 		Pattern pwdPattern = Pattern
 				.compile("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#&$%]).{8,12})");
@@ -29,7 +30,7 @@ public class UserLoginUtil {
 					cnt = 0;
 				}
 			}
-			if (cnt > 3 ) {
+			if (cnt > 3) {
 				errorMsg = "Invaild password: password cannot have more than 3 letters together";
 			}
 		} else {
@@ -37,21 +38,38 @@ public class UserLoginUtil {
 		}
 		return errorMsg;
 	}
-	
+
 	/**
 	 * User name validation implementation
+	 * 
 	 * @param userName
 	 * @return validation message
 	 */
-	public static String ValidateUserName ( String userName ){
-		
+	public static String ValidateUserName(String userName) {
+
 		String errorMsg = "";
-		Pattern namePattern = Pattern
-				.compile("^[a-zA-Z]{3,10}$");
+		Pattern namePattern = Pattern.compile("^[a-zA-Z]{3,10}$");
 		if (!namePattern.matcher(userName).matches()) {
-			errorMsg = "Invaild user name: user name has to be 3 - 10 characters" ;
+			errorMsg = "Invaild user name: user name has to be 3 - 10 characters";
 		}
 		return errorMsg;
 	}
 
+	public static String getServiceURL(String serviceName) {
+
+		String url = null;
+		Properties prop = new Properties();
+		try {
+			InputStream in = UserLoginUtil.class.getClassLoader()
+					.getResourceAsStream("url.properties");
+
+			prop.load(in);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		url = prop.getProperty(serviceName);
+
+		return url;
+	}
 }
